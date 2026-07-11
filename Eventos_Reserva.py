db77346085554a89a3053b6026dfaa11f7d1d965
floresJP <janeth.pascaciof@istpargentina.edu.pe>
2026-07-11 13:48:23 -0500

    SE AGREGO PATRÓN SINGLETON #1 — Logger Y EL PATRÓN SINGLETON #2 — SistemaConfig

--- Eventos_Reserva.py
+++ Eventos_Reserva.py
@@ -12,3 +12,52 @@
 import datetime
 import re
 from abc import ABC, abstractmethod
+# ──────────────────────────────────────────────────────────────────────────────
+# PATRÓN SINGLETON #1 — Logger
+# ──────────────────────────────────────────────────────────────────────────────
+class Logger:
+    _instancia = None
+
+    def __new__(cls):
+        if cls._instancia is None:
+            cls._instancia = super().__new__(cls)
+            cls._instancia._logs = []
+        return cls._instancia
+
+    def _registrar(self, nivel, mensaje):
+        hora = datetime.datetime.now().strftime("%H:%M:%S")
+        self._logs.append({"hora": hora, "nivel": nivel, "msg": mensaje})
+
+    def info(self, msg):    self._registrar("INFO",    msg)
+    def warning(self, msg): self._registrar("WARNING", msg)
+    def error(self, msg):   self._registrar("ERROR",   msg)
+
+    def mostrar_logs(self):
+        print(f"\n=== HISTORIAL DEL SISTEMA ({len(self._logs)} eventos) ===")
+        for log in self._logs:
+            print(f"  [{log['hora']}] {log['nivel']:7} | {log['msg']}")
+
+    def limpiar(self):
+        self._logs.clear()
+        print("  OK Historial de logs limpiado")
+        
+        # ──────────────────────────────────────────────────────────────────────────────
+# PATRÓN SINGLETON #2 — SistemaConfig
+# ──────────────────────────────────────────────────────────────────────────────
+class SistemaConfig:
+    _inst = None
+
+    def __new__(cls):
+        if cls._inst is None:
+            cls._inst = super().__new__(cls)
+            cls._inst.nombre  = "SISTEMA DE RESERVA DE EVENTOS"
+            cls._inst.version = "1.0"
+            cls._inst.empresa = "Reservafy"
+            cls._inst.autor   = "AMPUERO - PASCACIO"
+
+            Logger().info(
+                f"Sistema Iniciado: {cls._inst.nombre} "
+                f"Version: {cls._inst.version}"
+                f"Empresa: {cls._inst.empresa} "
+                f"Autor: {cls._inst.autor}")
+        return cls._inst
