de8f5cdb30979396ee12cf25c7cbc9f58c222773
Luca-istp <luca.ampueroc@istpargentina.edu.pe>
2026-08-03 01:30:06 -0500

    Se el  problema sobre con el  </main>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,18 +11,17 @@ function App() {
   return (
     <div className="d-flex">
       <Menu />
-      <main className="flex-grow-1"></main>
-      <Routes>
-        {/* Redirige la raíz "/" hacia "/inicio" */}
-        <Route path="/" element={<Navigate to="/inicio" replace />} /> 
-        <Route path="/inicio" element={<Panel />} />
-        <Route path="/clientes" element={<Clientes />} />
-        <Route path="/reservas" element={<Reservas />} />
-        <Route path="/tematicas" element={<Tematicas />} />
-        <Route path="/pagos" element={<Pagos />} />
-        <Route path="/cuotas" element={<Cuotas />} />
-
-      </Routes>
+      <main className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Navigate to="/inicio" replace />} /> 
+          <Route path="/inicio" element={<Panel />} />
+          <Route path="/clientes" element={<Clientes />} />
+          <Route path="/reservas" element={<Reservas />} />
+          <Route path="/tematicas" element={<Tematicas />} />
+          <Route path="/pagos" element={<Pagos />} />
+          <Route path="/cuotas" element={<Cuotas />} />
+        </Routes>
+      </main>
     </div>
   );
 }
